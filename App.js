84795839b96aee6fa613e356307c4f1ed192cd74
LeoBlanco123 <rm99119@gmail.com.br>
2024-03-07 22:20:33 -0300

    mudado view app.js

--- App.js
+++ App.js
@@ -1,32 +1,37 @@
-import {Text,View,SafeAreaView,ScrollView,Image,Button} from 'react-native'
-import Estilo from './components/Estilo'
+import React from 'react';
+import { Text, View, SafeAreaView, ScrollView, Image, Button } from 'react-native';
+import Estilo from './components/Estilo';
 
-export default ()=>(
+export default () => (
   <ScrollView>
     <View style={Estilo.viewSafeAndroid}>
       <SafeAreaView>
 
         <View style={Estilo.divPrin}>
           <Text style={Estilo.h1}>PIZZARIA FIAP</Text>
-          <Image style= {Estilo.tamImage}source={require('./assets/pizza.png')}/>
-        </View>  
-
-        <View>
-          <Text>Está com fome de Pizza?</Text>
-          <Text>Se sim Pizzaria FIAP, é o lugar perfeito para você!</Text>
+          <Image style={Estilo.tamImage} source={require('./assets/pizza.png')} />
         </View>
 
-        <View>
-          <Button title='Continue sem registrar' color={'#ff2200'}/>
+        <View style={Estilo.textPizza}>
+          <Text style={Estilo.textPizzaH1}>Está com fome de Pizza?</Text>
+          <Text style={Estilo.pizzaText}>Pizzaria FIAP é o lugar perfeito para você!</Text>
         </View>
 
-        <View>
-          <Button title='Registar' color={'#22ff00'}/>
-          <Button title='Login' color={'#d9d9d9'}/>
+        <View style={Estilo.buttonSemRegistro}>
+          <Button title='Continue sem registrar' color={'#ff2200'} />
+        </View>
+
+        <View style={Estilo.buttonContainer}>
+          <View style={Estilo.button}>
+            <Button title='Registrar' color={'#008000'} />
+          </View>
+
+          <View style={Estilo.button}>
+            <Button title='Login' color={'#808080'} />
+          </View>
         </View>
 
       </SafeAreaView>
     </View>
   </ScrollView>
-  
-)
+);
